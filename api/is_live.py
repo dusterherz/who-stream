af47906c673c938dbea7ab9eb4db872a55350882
Aude Lejuez <aude@derniercri.io>
2020-02-22 09:49:58 +0100

    Fix return value of is_live

--- api/is_live.py
+++ api/is_live.py
@@ -6,6 +6,7 @@ api = Namespace('is_live', description='Live operation')
 @api.route('/')
 @api.response(204, "Ok.")
 class IsLive(Resource):
+    @api.doc('is_live')
     def get(self):
         ''' Check if Mister MV is live '''
-        return api.response(204)
+        return '', 204
